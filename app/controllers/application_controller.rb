3a1904160bb234415cdfafd9372910045c2e5586
Tom Manderson <TRManderson@gmail.com>
2015-05-12 14:25:50 +1000

    Improved erroring out for Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,17 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
   end
+
+  if Rails.env.production?
+    unless Rails.application.config.consider_all_requests_local
+      rescue_from Exception, with: :render_500
+    end
+  end
+
+
+  def render_500(exception)
+    logger.info exception.backtrace.join("\n")
+    @log=exception.backtrace.join("<br />\n")
+    render template: 'errors/500', layout: 'layouts/application', status: 500
+  end
 end
