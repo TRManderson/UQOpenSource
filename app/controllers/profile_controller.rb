15590469f2286f1dcf2c88e12c06c9c25f612482
Tom Manderson <TRManderson@gmail.com>
2015-05-14 11:24:16 +1000

    Nonexistant users are handled nicely

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,12 @@
 class ProfileController < ApplicationController
   def show
     @user = User.find params[:id]
+    rescue ActiveRecord::RecordNotFound => e
+      flash[:error]="The user you tried to access doesn't exist"
+      if request.referrer
+        redirect_to request.referrer
+      else
+        redirect_to root_path
+      end
   end
 end
